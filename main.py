3899dfa1a0db3239fb487986fbfcf7f6064d873e
TheShadowKing19 <mikiwolf19@gmail.com>
2022-04-08 13:41:04 +0200

    .gitignore + początek i skończona funkcja wbudowana

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import pandas as ps
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def wiPCA(tablica_wartosci):
+    tablica_usredniona = tablica_wartosci - np.mean(tablica_wartosci, axis=0)   # Uśredniamy wartości tablicy
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return
+
+
+tablica = np.random.rand(20, 20)
+tablica = tablica.reshape(200, 2)
+plt.scatter(tablica[:, [0]], tablica[:, [1]])
+plt.show()
+
+# Funkcja wbudowana
+pca = PCA(n_components=1)
+f_wbudowana = pca.fit(tablica).transform(tablica)
+print(f_wbudowana)
+plt.scatter(f_wbudowana, f_wbudowana)
+plt.show()
